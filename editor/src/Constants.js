07607fdbe89e4384e34cb664b610838655a5c443
fucever <fiveoutlines@163.com>
2020-06-08 09:51:37 +0800

    none

--- editor/src/Constants.js
+++
@@ -1,3 +0,0 @@
-export default class {
-    static BUTTOM_SLIDER_FADE_TIMEOUT = "BUTTOM_SLIDER_FADE_TIMEOUT";
-}
--- editor/src/Snippet.js
+++
@@ -1,8 +0,0 @@
-import Element from "./Element";
-
-export default class extends Element{
-    constructor(global){
-        super(global.creatElemt('div'), global);
-        
-    }
-}
