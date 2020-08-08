36e973011075ac454b2de1f470122262d456b097
savaltorehungca <guichuanh@outlook.com>
2020-08-08 20:53:18 +0800

    none

--- packages/editor/src/textinput/keydown/paragraphKeyDownBehavior.ts
+++ packages/editor/src/textinput/keydown/paragraphKeyDownBehavior.ts
@@ -1,18 +1,27 @@
-import { Editor } from "../..";
+import { Editor } from "../../Editor";
 import { KeyDownBehavior } from "../../behaviorTypes";
 import { isHotkey } from 'is-hotkey';
 import { ct } from "utils";
-import { Point } from "editor-core";
+import { Point, Selection } from "editor-core";
 
 export function paragraphKeyDownBehavior(editor: Editor): KeyDownBehavior {
     return (event, selection) => {
         const point: Point = ct(selection.end);
-        if(isHotkey('ArrowLeft', event)){
-            arrowLeft(point);
+        if (isHotkey('ArrowLeft', event)) {
+            arrowLeft(editor, point, selection);
+        }
+        else if(isHotkey('ArrowRight', event)){
+            arrowRight(editor, point, selection);
         }
     }
 }
 
-function arrowLeft(point: Point){
+function arrowLeft(editor: Editor, point: Point, selection: Selection) {
+    point.offset -= 1;
+    editor.docTree.changeSelection(point, point);
+}
 
+function arrowRight(editor: Editor, point: Point, selection: Selection) {
+    point.offset += 1;
+    editor.docTree.changeSelection(point, point);
 }
