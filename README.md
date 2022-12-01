42b1572f9a6dc451bc6692bc3ea5ac892ba8443f
fucever <fiveoutlines@163.com>
2022-12-01 10:51:54 +0800

    none

---
+++ README.md
@@ -0,0 +1,18 @@
+# 一款彻底自定义的编辑器
+设计理念源于日常使用OneNote，但OneNote的数据不在本地，故有了此项目制作的想法。  
+
+基础效果展示：
+![](md_data/presence.png)
+
+运行方式：
+npm 或 yarn，以npm为例：  
+```
+npm install
+npm run build
+npm run serve
+```
+然后访问 localhost:3000
+
+>! 本项目已经不再开发，虽然方式可行，但如果最终制作出来也只是扩展功能比OneNote稍好一点的复制品，且所有功能由于都是原生实现，耗时过多，故不再开发。
+
+>后续将启动新项目基于markdown实现
--- packages/client/src/testdata.ts
+++ packages/client/src/testdata.ts
@@ -4,16 +4,43 @@ export var editorcontent = [
     {
         type: 'paragraph',
         indentation: 2,
+        content: "一款彻底自定义的编辑器，不是基于浏览器的contexeditable属性，以下是功能介绍："
+    },
+    {
+        type: 'table',
+        indentation: 3,
+        content: [
+            [
+                {
+                    type: 'paragraph',
+                    indentation: 2,
+                    content: "文字排版"
+                },
+                {
+                    type: 'table',
+                    content: [
+                        [{ type: 'paragraph', content: '完全自定义，对于每行文字完全自己计算生成，因此可以实现更多自定义功能，所有均可以所见即所得的方式编辑' }],
+                        [{ type: 'paragraph', content: '选区功能，也是完全自定义，不是依赖浏览器的Selection接口，因此可实现vscode那样的多行选择，多行同时编辑以及更多自定义功能' }]
+                    ]
+                }
+            ],
+            [
+                { type: 'paragraph', content: '是否支持数学公式' },
+                {
+                    type: 'paragraph',
+                    content: '支持，利用ketex进行渲染'
+                }
+            ]
+        ]
+    },
+    {
+        type: 'paragraph',
+        indentation: 2,
         content: "当且仅当[[formula(c = \\pm\\sqrt{a^2 + b^2})]]成立"
     },
     {
         type: 'paragraph',
-        content: "内嵌代码框[[code(http://baidu.com)]], 我的附件[[attachment(http://baidu.com)]]在这里呢"
-    },
-    {
-        type: 'paragraph',
-        indentation: 1,
-        content: "工业和信息化部、财政部、商务部等5部门22日联合发布了修改后的乘用车企业平均燃料消耗量与新能源汽车积分并行管理办法，也就是常说的“双积分”管理办法。新办法更大力度提升了对企业节能减排方面的优惠政策，使汽车企业节能减排更“有利可图”"
+        content: "内嵌代码框[[code(http://baidu.com)]], 我的附件[[attachment(http://baidu.com)]],点击编辑附件"
     },
     {
         type: 'paragraph',
@@ -34,8 +61,8 @@ export var editorcontent = [
                 indentation: 2,
                 content: "       工业和信息化部、财政部、商务部等5部门22日联合发布了修改后的乘用车企业平均燃料消耗量与新能源汽车积分并行管理办法，也就是常说的“双积分”管理办法。新办法更大力度提升了对企业节能减排方面的优惠政策，使汽车企业节能减排更“有利可图”"
             }, {
-                type: 'table',
-                content: [[{ type: 'paragraph', content: 'shit' }]]
+                type: 'paragraph',
+                content: 'hah'
             }],
             [{ type: 'paragraph', content: '3' },
             {
@@ -229,7 +256,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -243,7 +270,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -257,7 +284,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -271,7 +298,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -285,7 +312,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -299,7 +326,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
@@ -313,7 +340,7 @@ export var nodelist = {
             tag: '🍕',
             title: 'xixi',
             children: []
-        },{
+        }, {
             tag: '🍕',
             title: 'xixi',
             children: []
